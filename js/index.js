59122d6563e57b59915e2e3bf3de14153f45eddd
VictorSB <vsb.vih@gmail.com>
2022-12-01 22:45:56 -0300

    Adicionado background e estilo do menu com JS

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 const abasMenu = document.querySelectorAll('a')
 const abas = document.querySelectorAll('.abas')
+const menu = document.querySelectorAll('.menu')
 
 abasMenu.forEach(function (menu, index) {
     menu.addEventListener("click", function () {
@@ -7,7 +8,23 @@ abasMenu.forEach(function (menu, index) {
         const esconderCardapio = document.querySelector('.mostrar')
         if (esconderCardapio) {
             esconderCardapio.classList.remove('mostrar')
-        } 
-        abas[index].classList.add('mostrar') 
+        }
+        abas[index].classList.add('mostrar')
     })
 })
+
+menu.forEach(function (selecionado) {
+    selecionado.addEventListener('click', function () {
+        const menuSelecionado = document.querySelector('.selecionado')
+        if (menuSelecionado) {
+            menuSelecionado.classList.remove('selecionado')
+        }
+        selecionado.classList.add('selecionado')
+
+
+    })
+})
+
+
+
+
