40704447b2133e8bd788b8001183d0681d32c39d
VictorSB <vsb.vih@gmail.com>
2022-12-08 19:50:56 -0300

    Corrigido erro nos botões das redes sociais

--- js/index.js
+++ js/index.js
@@ -2,26 +2,16 @@ const abasMenu = document.querySelectorAll('a')
 const abas = document.querySelectorAll('.abas')
 const menu = document.querySelectorAll('.menu')
 
+menu.forEach((aba,index)=>{  
+    aba.addEventListener('click', ()=>{
+        const abaClicada = document.querySelector('.selecionado') // Pego no menu o elemento que possui a classe selecionado
+        abaClicada.classList.remove('selecionado') //removo a classe
 
-abasMenu.forEach(function (menu, index) {
-    menu.addEventListener("click", function () {
-        const esconderCardapio = document.querySelector('.mostrar')
-        
-        if (esconderCardapio) {
-            esconderCardapio.classList.remove('mostrar')
-        }
-        abas[index].classList.add('mostrar')
-    })
-})
+        const abaMostrada = document.querySelector('.mostrar') // Pego no menu o elemento que possui a classe mostrar das abas
+        abaMostrada.classList.remove('mostrar') //removo a classe
 
-menu.forEach(function (selecionado) {
-    selecionado.addEventListener('click', function () {
-        const menuSelecionado = document.querySelector('.selecionado')
-        
-        if (menuSelecionado) {
-            menuSelecionado.classList.remove('selecionado')
-        }
-        selecionado.classList.add('selecionado')
+        aba.classList.toggle("selecionado"); //Na próxima aba do menu já adiciono a class selecionado
+        abas[index].classList.toggle("mostrar"); //Na próxima seção adiciono a classe mostrar, baseado no índice
     })
 })
 
